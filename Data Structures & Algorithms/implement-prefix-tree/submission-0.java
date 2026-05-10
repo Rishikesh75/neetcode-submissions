31bf7c87cff99a96b2938520a2c75654a2dc47f1
Rishikesh <119777850+Rishikesh75@users.noreply.github.com>
2026-05-10 22:28:31 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,84 @@
+class PrefixTree {
+
+    class Node {
+        Node[] links = new Node[26];
+        boolean flag = false;
+
+        boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        Node get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node node) {
+            links[ch - 'a'] = node;
+        }
+
+        void setEnd() {
+            flag = true;
+        }
+
+        boolean isEnd() {
+            return flag;
+        }
+    }
+
+    private Node root;
+
+    public PrefixTree() {
+        root = new Node();
+    }
+
+    // Insert word into Trie
+    public void insert(String word) {
+        Node node = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (!node.containsKey(ch)) {
+                node.put(ch, new Node());
+            }
+
+            node = node.get(ch);
+        }
+
+        node.setEnd();
+    }
+
+    // Search complete word
+    public boolean search(String word) {
+        Node node = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (!node.containsKey(ch)) {
+                return false;
+            }
+
+            node = node.get(ch);
+        }
+
+        return node.isEnd();
+    }
+
+    // Check prefix exists
+    public boolean startsWith(String prefix) {
+        Node node = root;
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+
+            if (!node.containsKey(ch)) {
+                return false;
+            }
+
+            node = node.get(ch);
+        }
+
+        return true;
+    }
+}
