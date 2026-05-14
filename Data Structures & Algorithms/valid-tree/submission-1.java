ce4fce4c8ac7240c1f9018342ad58430c89becab
Rishikesh <119777850+Rishikesh75@users.noreply.github.com>
2026-05-14 10:44:32 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    public boolean dfs(
+            int node,
+            int parent,
+            List<List<Integer>> graph,
+            boolean[] visited
+    ) {
+
+        visited[node] = true;
+
+        for (int adjNode : graph.get(node)) {
+
+            // skip the node we came from
+            if (adjNode == parent) continue;
+
+            // cycle detected
+            if (visited[adjNode]) return false;
+
+            if (!dfs(adjNode, node, graph, visited)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean validTree(int n, int[][] edges) {
+
+        // tree must have exactly n-1 edges
+        if (edges.length != n - 1) return false;
+
+        // create graph
+        List<List<Integer>> graph = IntStream.range(0,n)
+                                    .mapToObj(i -> new ArrayList<Integer>())
+                                    .collect(Collectors.toList());
+                        
+
+        for (int[] edge : edges) {
+
+            int u = edge[0];
+            int v = edge[1];
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        // check cycle
+        if (!dfs(0, -1, graph, visited)) {
+            return false;
+        }
+
+        // check connected
+        for (boolean nodeVisited : visited) {
+            if (!nodeVisited) return false;
+        }
+
+        return true;
+    }
+}
