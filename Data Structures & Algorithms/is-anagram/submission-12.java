5c1756fc81d681601a861753184e9481931dfd9f
Rishikesh <119777850+Rishikesh75@users.noreply.github.com>
2026-09-05 06:44:53 +0530

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //TC:O(N) SC:O(1)
+        int[] hashArr = new int[26];
+
+        for(var ch : s.toCharArray()){
+            hashArr[(ch-'a')]++;
+        }
+
+        for(var ch : t.toCharArray()){
+            if(hashArr[(ch-'a')] == 0)
+                return false;
+            hashArr[(ch-'a')]--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(hashArr[i]!=0)
+                return false;
+        }
+        return true;
+    }
+}
