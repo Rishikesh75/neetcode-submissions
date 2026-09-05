701544172e5881e79c0c8d9366beb29a9eb27e36
Rishikesh <119777850+Rishikesh75@users.noreply.github.com>
2026-09-05 16:49:57 +0530

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        int[] need = new int[128];
+        int[] window = new int[128];
+
+        // Count characters required from t
+        for (char c : t.toCharArray()) {
+            need[c]++;
+        }
+
+        int left = 0;
+        int count = 0;
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+            window[c]++;
+
+            // This character is required and we haven't
+            // exceeded the required frequency
+            if (need[c] > 0 && window[c] <= need[c]) {
+                count++;
+            }
+
+            // Window is valid
+            while (count == t.length()) {
+
+                // Update answer
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                window[leftChar]--;
+
+                // Removing this character makes window invalid
+                if (need[leftChar] > 0 &&
+                    window[leftChar] < need[leftChar]) {
+                    count--;
+                }
+
+                left++;
+            }
+        }
+
+        if (minLen == Integer.MAX_VALUE) {
+            return "";
+        }
+
+        return s.substring(start, start + minLen);
+    }
+}
